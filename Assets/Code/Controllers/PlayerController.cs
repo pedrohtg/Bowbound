973c77e1f7257f31dc43028966b7510f8a568a1c
PGPereira <pedrogabriel@dcc.ufmg.br>
2017-07-25 15:53:49 -0300

    controle ganhando forma

--- Assets/Code/Controllers/PlayerController.cs
+++ Assets/Code/Controllers/PlayerController.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
-	// Use this for initialization
 	void Start () {
 		Debug.LogWarning ("Quero pegar blablaba");	
 
-		if (Application.platform == RuntimePlatform.LinuxEditor || Application.platform == RuntimePlatform.LinuxPlayer) {
-			Debug.LogWarning ("Estou no linux!");
-		} else if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer) {
-			Debug.LogWarning ("Estou no windows!");
-		} else if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer) {
-			Debug.LogWarning ("Estou no OSX!");
-		}
+		inputJoystick ();
+		inputKeyboard ();
 	}
 	
 	// Update is called once per frame
@@ -22,8 +15,37 @@ public class PlayerController : MonoBehaviour {
 		
 	}
 
-	public static bool IsUnix()	{
-		var platform = (int)System.Environment.OSVersion.Platform;
-		return (platform == 4) || (platform == 6) || (platform == 128);
+	void inputKeyboard() {
+		float UP, DOWN, LEFT, RIGHT;
+		UP = Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.W);
+		DOWN = Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.S);
+		LEFT = Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.A);
+		RIGHT = Input.GetKeyDown (KeyCode.RightArrow) || Input.GetKeyDown (KeyCode.D);
+
+		if (UP || DOWN) {
+			if (UP) {
+				
+			}	
+			if (DOWN) {
+				
+			}
+		} else if (LEFT || RIGHT) {
+			if (LEFT) {
+				
+			}
+			if (RIGHT) {
+				
+			}
+		}
+	}
+
+	void inputJoystick() {
+		if (Application.platform == RuntimePlatform.LinuxEditor || Application.platform == RuntimePlatform.LinuxPlayer) {
+			Debug.LogWarning ("Estou no linux!");
+		} else if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer) {
+			Debug.LogWarning ("Estou no windows!");
+		} else if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer) {
+			Debug.LogWarning ("Estou no OSX!");
+		}
 	}
 }
