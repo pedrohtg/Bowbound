592312d2104dc579d640cdefda307669f37e8f9d
paniago96 <p.p.paniago@hotmail.com>
2017-07-25 16:12:30 -0300

    Terrain and Resources created

---
+++ Assets/Code/Controllers/BulletController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	public static GroundController groundController; //controlador do terreno
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
