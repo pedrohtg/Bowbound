cf37baa3ae87e08e77c7a20a3d19b4f6f2c7736c
PGPereira <pedrogabriel@dcc.ufmg.br>
2017-07-25 15:06:57 -0300

    Teste de SO

--- Assets/Code/Controllers/PlayerController.cs
+++ Assets/Code/Controllers/PlayerController.cs
@@ -6,11 +6,23 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.LogWarning ("Quero pegar blablaba");	
+
+		if (IsUnix ()) {
+			Debug.LogWarning ("Estou em um sistema Unix");	
+		} else {
+			Debug.LogWarning ("Estou em um sistema Windows");
+		}
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public static bool IsUnix()	{
+		var platform = (int)System.Environment.OSVersion.Platform;
+		return (platform == 4) || (platform == 6) || (platform == 128);
 	}
 }
