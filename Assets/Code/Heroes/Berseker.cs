35b4a8726d2403d4ef406af992d63ee60caf7ad0
PGPereira <pedrogabriel@dcc.ufmg.br>
2017-07-26 04:17:52 -0300

    Esqueleto das classes

---
+++ Assets/Code/Heroes/Berseker.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31;
+
+public class Berseker : HeroController {
+
+	public static int Health = 200;
+	public static int Speed = 5;
+	public static float DmgCausedMultiplier = .6f;
+	public static float DmgReceivedMultiplier = .4f;
+	public static float Velocity = 1.0f;
+
+	public static int RequiredEnergyForSkill2 = 40;
+	public static int RequiredEnergyForSkill3 = 60;
+	public static int RequiredEnergyForSkill4 = 100;
+
+	//Initialise
+	public override void Initialise(){
+		_health = Health;
+		_energy = 0;
+		_speed = Speed;
+		_dir = true;
+		_dmgCausedMultiplier = DmgCausedMultiplier;
+		_dmgReceivedMultiplier = DmgReceivedMultiplier;
+		_angle = 0;
+		_launchForce = 0;
+		ch = GetComponent<CharacterController2D> ();
+		_velocity = Velocity;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override bool CanUseSkill2(){
+		return _energy >= RequiredEnergyForSkill2;
+	}
+
+
+	public override bool CanUseSkill3(){
+		return _energy >= RequiredEnergyForSkill3;
+	}
+
+
+	public override bool CanUseSkill4(){
+		return _energy >= RequiredEnergyForSkill4;
+	}
+
+	public override void UseSkill1(){
+		
+	}
+
+	public override void UseSkill2(){
+		
+	}
+
+	public override void UseSkill3(){
+		
+	}
+
+	public override void UseSkill4(){
+		
+	}
+
+}
